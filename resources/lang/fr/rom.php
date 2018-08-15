bf580f491b6ee9fbe1426b75c2fef7aba732285d
sporchia <print@blindchaos.net>
2018-08-14 22:55:15 -0400

    typo

--- resources/lang/fr/rom.php
+++ resources/lang/fr/rom.php
@@ -13,7 +13,7 @@ return [
 	'info' => [
 		'logic' => __('randomizer.logic.title'),
 		'build' => 'Création de ROM',
-		'difficulty' => _('randomizer.difficulty.title'),
+		'difficulty' => __('randomizer.difficulty.title'),
 		'variation' => __('randomizer.variation.title'),
 		'shuffle' => __('entrance.shuffle.title'),
 		'mode' => __('randomizer.mode.title'),
